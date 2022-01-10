77f171ed0cb0dab46bace9d92b0fa3616c712601
Jack Tysoe <64026310+ttyS0e@users.noreply.github.com>
2022-01-10 16:18:02 +0000

    Update allow-overriding-ingress-class.sh

--- patches/inso/allow-overriding-ingress-class.sh
+++ patches/inso/allow-overriding-ingress-class.sh
@@ -9,6 +9,7 @@ CONTAINER_ID=$(docker run -dt --rm -v $(pwd)/out:/host --name inso-builder alpin
 
 # Install requirements
 docker exec -it $CONTAINER_ID ash -c "apk add --update nodejs npm libcurl"
+docker exec -it $CONTAINER_ID ash -c "npm install --global pkg insomnia-inso"
 
 # Patch Inso
 docker exec -it $CONTAINER_ID ash -c "cd /usr/local/lib/node_modules/insomnia-inso/ && cat << EOF > script.ed
@@ -18,13 +19,10 @@ docker exec -it $CONTAINER_ID ash -c "cd /usr/local/lib/node_modules/insomnia-in
 106d
 w
 q
-
 EOF"
-
 docker exec -it $CONTAINER_ID ash -c "cd /usr/local/lib/node_modules/insomnia-inso/ && ed node_modules/openapi-2-kong/dist/kubernetes/generate.js < script.ed"
 
 # Build Inso standalone executables
-docker exec -it $CONTAINER_ID ash -c "npm install --global pkg insomnia-inso"
 docker exec -it $CONTAINER_ID ash -c "cd /usr/local/lib/node_modules/insomnia-inso/ && pkg . --target node14-alpine-x64 && mv insomnia-inso insomnia-inso-alpinelinux"
 docker exec -it $CONTAINER_ID ash -c "cd /usr/local/lib/node_modules/insomnia-inso/ && pkg ."
 
